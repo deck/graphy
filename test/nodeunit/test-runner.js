d44c3bef76e8cb677eff0296ce2113bcf7be7604
Joel Birchler <joel@deckmonitoring.com>
2012-08-18 00:21:49 -0700

    regression tests added to runner

--- test/nodeunit/test-runner.js
+++ test/nodeunit/test-runner.js
@@ -1,22 +1,36 @@
 // Run via phantomJS
 
 var page = require('webpage').create();
-
 page.settings.userAgent = 'phantom';
+
+var runners = [
+	{ url: 'http://localhost:8374/test/sanity/run.html', title: 'Sanity Tests' },
+	{ url: 'http://localhost:8374/test/regression/run.html', title: 'Regression Tests' }
+]
+
+var openNextRunner = function() {
+	if (runners.length == 0) { phantom.exit(); }
+	
+	var runner = runners.shift();
+	console.log("*** " + runner.title + " ***");
+	
+	page.open(runner.url, function (status) {
+		if (status == "fail") { 
+			console.log("Status: " + status);
+			phantom.exit(8); 
+		}
+	});
+}
 
 page.onConsoleMessage = function (msg) {
 	if (msg == "HELLO PHANTOM: Tests Failed.") {
 		phantom.exit(1);
 	} else if (msg == "HELLO PHANTOM: Tests Successful.") {
-		phantom.exit();
+		console.log("");
+		openNextRunner();
 	} else {
 		console.log(msg); 
 	}
 }
 
-page.open('http://localhost:8374/test/sanity/run.html', function (status) {
-	if (status == "fail") { 
-		console.log("Status: " + status);
-		phantom.exit(8); 
-	}
-});
+openNextRunner();
