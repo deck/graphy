5ab514bfa16ff32d0bf851c98c473db4c452bf46
Joel Birchler <joel@joelbirchler.com>
2012-09-12 23:51:02 -0700

    moved from phantom-jasmine to aladdin and added package.json for npm install

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cat source/graphy.core.js source/graphy.filters.js source/graphy.formatters.js source/graphy.interval.js source/graphy.util.js source/graphy.color.js source/graphy.renderers.js > graphy.js
 cat source/require-prefix.js graphy.js source/require-suffix.js > require-graphy.js
-phantom-jasmine
+[ -f aladdin ] && aladdin || node node_modules/aladdin/bin/aladdin
