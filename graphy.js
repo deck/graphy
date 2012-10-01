d882242d624ae96ea6a8db5d56dce6caa97aaaff
Joel Birchler <joel@joelbirchler.com>
2012-10-01 11:44:54 -0700

    added DECK copyright to graphy.color

--- graphy.js
+++ graphy.js
@@ -1208,6 +1208,7 @@ Graphy.util = {
 /*
 *  Copyright 2005 Sabre Airline Solutions
 *  Copyright 2005-2008 Sam Stephenson
+*  Copyright 2010-2012 DECK Monitoring LLC.
 *
 *  --
 *
@@ -1251,6 +1252,22 @@ Graphy.util = {
 *  - Removed Rico.Color.createColorFromBackground, Rico.Color.createGradientV, Rico.Color.
 *    createGradientH, Rico.Color.createGradientContainer, Rico.Color.createColorPath, 
 *    Rico.Color.setColorHue and anything outside of Rico.Color.
+*
+*  --
+* 
+*  Copyright 2010-2012 DECK Monitoring LLC.
+*
+*  This file is part of Graphy from DECK Monitoring LLC.
+*
+*  Graphy is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser Public License
+*  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later 
+*  version.
+*
+*  Graphy is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+*  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
+*
+*  You should have received a copy of the Lesser General Public License along with Graphy. If not, see 
+*  <http://www.gnu.org/licenses/>.
 *
 */
 Graphy.Color = (function() {
