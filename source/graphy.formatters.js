3db782f7d017fbf3bf0c982e9d8a2888f6d2dabf
Earl Swigert <earl@periscopic.com>
2012-11-30 17:12:18 -0800

    Added shortDate, fixedOne, fixedTwo, integer, and usMoney to formatting options.

--- source/graphy.formatters.js
+++ source/graphy.formatters.js
@@ -17,20 +17,20 @@ Graphy.formatters = {
   humanDate: function(val, precision) {
     var date = new Date(val);
     precision = precision || 1;
-   
+
     if ( isNaN( date.getTime() ) ) {
       val = "";
-    } else if ( precision < Graphy.interval.month && date.getHours() == 12 && date.getMinutes() == 0 ) {
+    } else if ( precision < Graphy.interval.month && date.getHours() == 12 && date.getMinutes() === 0 ) {
       val = "noon";
     } else if ( precision < Graphy.interval.month && date.getHours() || date.getMinutes() || date.getSeconds()  ) {
       var suffix = "am";
       var hours = date.getHours();
-      if ( hours >= 12 ) { 
+      if ( hours >= 12 ) {
         suffix = "pm";
         if ( hours > 12 ) {
           hours -= 12;
         }
-      } else if ( hours == 0 ) {
+      } else if ( hours === 0 ) {
         hours = 12;
       }
       var minutes = date.getMinutes();
@@ -42,11 +42,63 @@ Graphy.formatters = {
     } else {
       val = Graphy.formatters.months[ date.getMonth() ] + " " + date.getFullYear();
     }
-   
     return val;
   },
+
+  shortDate: function(val, precision) {
+    var date = new Date(val);
+    precision = precision || 1;
+    
+    if ( isNaN( date.getTime() ) ) {
+      val = "";
+    } else if ( precision < Graphy.interval.month && date.getHours() == 12 && date.getMinutes() === 0 ) {
+      val = "12:00";
+    } else if ( precision < Graphy.interval.month && (date.getHours() || date.getMinutes() || date.getSeconds()) ) {
+      var hours = date.getHours();
+      var minutes = date.getMinutes();
+      if ( minutes < 10 ) { minutes = "0" + minutes; }
+      val = hours + ":" + minutes;
+    } else if ( precision < Graphy.interval.month && date.getDate() ) {
+      var day = date.getDate();
+      if(day < 10) { day = "0" + day; }
+      var month = date.getMonth() + 1;
+      if(month < 10) { month = "0" + month; }
+      val = month + "/" + day;
+    } else {
+      var month = date.getMonth() + 1;
+      if(month < 10) { month = "0" + month; }
+      var year = date.getFullYear();
+      year = year.toString().substr(2, 2);
+      val = month + "/01/" + year;
+    }
+    return val;
+  },
+
+  fixedTwo: function(val, precision) {
+    return val.toFixed(2);
+  },
+
+  fixedOne: function(val, precision) {
+    return val.toFixed(1);
+  },
+
+  integer: function(val, precision) {
+    return parseInt(val, 10);
+  },
+  
+  usMoney: function(val, precision) {
+    var c = 2,
+        d = ".",
+        t = ",",
+        s = val < 0 ? "-" : "",
+        i = parseInt(val = Math.abs(+val || 0).toFixed(c), 10) + "",
+        j = (j = i.length) > 3 ? j % 3 : 0;
+    return s + "$" + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(val - i).toFixed(c).slice(2) : "");
+  },
+
+  none: function(val, precision){
+    return " ";
+  },
  
-  none: function(val, precision){ return "" },
- 
-  months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"] 
+  months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 }
